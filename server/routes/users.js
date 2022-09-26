4ce967a9f59f1409d2f9728a3b2d71a5a7c4473a
Muratcan Tüfekçi <muratcan.tufekci@pixelplus.net>
2022-09-26 17:44:23 +0300

    first commit

---
+++ server/routes/users.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {  } from '../controllers/user.js';
+
+const router = express.Router();
+
+//update user
+//delete user
+//get a user
+//subscribe a user
+//unsubscribe a user
+//like a video
+//dislike a videı
+
+export default router;
